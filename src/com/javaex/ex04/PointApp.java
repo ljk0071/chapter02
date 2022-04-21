09f3a7c5ccf418124b09a3e02f33707d6f0d558c
ljk0071 <ssobbs13@naver.com>
2022-04-21 18:14:08 +0900

    1.3

--- src/com/javaex/ex04/PointApp.java
+++ src/com/javaex/ex04/PointApp.java
@@ -10,16 +10,16 @@ public class PointApp {
 		int x,y;
 		x=y=0;
 		while(true) {
-			System.out.print("(종료하시려면 0을 입력하세요)\nX좌표를 입력 해 주세요: ");
+			System.out.print("끝내시려면 x,y값에 0을 넣어주세요\nX좌표를 입력 해 주세요: ");
 			x=sc.nextInt();
-			System.out.print("(종료하시려면 0을 입력하세요)\nY좌표를 입력 해 주세요: ");
+			System.out.print("Y좌표를 입력 해 주세요: ");
 			y=sc.nextInt();
-			if (x == 0 || y == 0) {
+			if ( x==0 && y ==0 ) {
 				break;
-			}else {
+			}else { 
 				xy.setX(x);
-				xy.setY(y);
-				xy.draw();
+			xy.setY(y);
+			xy.draw();
 			}
 		}
 		sc.close();
